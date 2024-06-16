5e53093994df3f047d6753c0b2c7a54c808be246
jnkmtsd <admin@gleampine.com>
2024-06-16 23:38:13 +0900

    FREELANCE-13: モックとスタブのサンプルテスト

---
+++ src/StubMockSample/UserDatabaseClient.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\StubMockSample;
+
+class UserDatabaseClient
+{
+    public function connectInfo(): array
+    {
+        // 本来は動的
+        return [
+            'dbms' => 'mysql',
+            'host' => 'localhost',
+            'dbname' => 'test',
+            'charset' => 'utf8',
+        ];
+    }
+
+    public function register(string $name, string $email): array
+    {
+        // 本来はデータベースにデータを登録する処理が入る
+        return [
+            'id' => 'A123',
+            'name' => $name,
+            'email' => $email,
+        ];
+    }
+}
